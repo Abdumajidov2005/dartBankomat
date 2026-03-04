7fbbaf8213ac49e9eb374b97948d25adf8771cdd
Abdumajidov2005 <otkirbekabdumajidov28@gmail.com>
2026-03-04 22:47:10 +0500

    file

---
+++ main.dart
@@ -0,0 +1,51 @@
+void main(List<String> args) {
+  // List sonlar = [20, 30, 10];
+  // List raqamlar = [...sonlar, 10, ...sonlar, 20, 40];
+  // print(raqamlar);
+  // sonlar.sort();
+  // print(sonlar);
+  // sonlar.sort((a, b) => b.compareTo(a));
+  // print(sonlar);
+
+  // for (int i = 0; i < sonlar.length; i++) {
+  //   if (sonlar[i] == 10) {
+  //     print(sonlar[i]);
+  //     break;
+  //   } else {
+  //     sonlar.add(10);
+  //     print(sonlar[i]);
+  //   }
+  // }
+
+  // if (sonlar.contains(10)) {
+  //   print(sonlar);
+  // } else {
+  //   sonlar.add(10);
+  //   print("sizda 10 soni yo'q edi qo'shib berdim ---  $sonlar");
+  // }
+  // num count = 0;
+
+  // for (int i = 0; i < sonlar.length; i++) {
+  //   count +=  sonlar[i];
+  // }
+
+  // print(count);
+  // sonlar.add(30);
+  // sonlar.remove(20);
+  // sonlar.removeAt(2);
+  // sonlar.clear();
+  // sonlar.isEmpty;
+  // sonlar.isNotEmpty;
+  // sonlar.contains(10);
+
+  // if (sonlar.length >= 5) {
+  //   print(sonlar);
+  // } else {
+  //   sonlar.add(50);
+  //   print(sonlar);
+  // }
+
+
+
+
+}
